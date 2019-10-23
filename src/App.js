4f4e74b4f4619dc17bd4f8d5059fb7bc8794425d
PhuChau <vinhphu101195@gmail.com>
2019-10-23 14:15:09 +0300

    build header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPageScroller from "react-page-scroller";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Home from "./Component/Home/HomeComponent";
+import Profile from "./Component/Profile/ProfileComponent";
+
+import Third from "./Third";
+import Fourth from "./Fourth";
+import Fifth from "./Fifth";
+
+import "./index.css";
+
+export default class FullPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentPage: 1 };
+    this._pageScroller = null;
+  }
+
+  goToPage = (eventKey) => {
+    this._pageScroller.goToPage(eventKey);
+  };
+
+  pageOnChange = (number) => {
+    this.setState({ currentPage: number });
+  };
+
+
+
+  render() {
+    return <React.Fragment>
+      <ReactPageScroller ref={c => this._pageScroller = c} pageOnChange={this.pageOnChange}>
+        <Home />
+        <Profile />
+        <Third />
+        <Fourth goToPage={this.goToPage} />
+        <Fifth />
+      </ReactPageScroller>
+
+    </React.Fragment>
+  }
 }
-
-export default App;
---
+++ src/Component/Home/HomeComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+import logo from "../../logo.svg";
+
+const HomeComponent = () => {
+    return (
+        <div>
+            <header className="App-header ">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="header-infor">
+                    <h1>Phu Chau</h1>
+                    <h2>"Maximum Effort" </h2>
+                    <button to="/profile" className="btn btn--green  ">
+                        Discover More
+                    </button>
+                </div>
+            </header>
+            <div className="scroll  bottom-project" />
+        </div>
+    );
+}
+
+export default HomeComponent;
+
