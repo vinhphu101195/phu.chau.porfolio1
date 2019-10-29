1edd3e57c7fc9229e640965391ffb067e324f0f2
Phu Chau <vinhphu101195@gmail.com>
2019-10-29 15:07:24 +0200

    build project components

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -1,38 +1,49 @@
 import React, { useContext, useState } from "react";
+import "./Project.css";
 import SmallProject from "./SmallProject";
 import { ProjectContext } from "../../contexts/ProjectContexts";
 // import Projectpopup from "./ProjectPopup";
 
 const Projects = () => {
-    const { projectObject } = useContext(ProjectContext);
-    const [popup, setPopup] = useState({});
+  const { projectObject } = useContext(ProjectContext);
+  const [popup, setPopup] = useState({});
 
-    const onClickPopup = data => {
-        setPopup(data);
-    };
+  const onClickPopup = data => {
+    setPopup(data);
+  };
 
-    const showProjects = projects => {
-        if (projects.length > 0) {
-            return projects.map((project, index) => {
-                return (
-                    <SmallProject
-                        key={index}
-                        project={project}
-                        index={index}
-                        onClickPopup={data => onClickPopup(data)}
-                    ></SmallProject>
-                );
-            });
-        }
-    };
+  const showProjects = projects => {
+    if (projects.length > 0) {
+      return projects.map((project, index) => {
+        return (
+          <SmallProject
+            key={index}
+            project={project}
+            index={index}
+            onClickPopup={data => onClickPopup(data)}
+          ></SmallProject>
+        );
+      });
+    }
+  };
 
-    return (
-        <section className="projects">
-            {/* {showProjects(projectObject)} */}
-            {/* {<Projectpopup project={popup}></Projectpopup>} */}
+  const showProjectsName = projects => {
+    if (projects.length > 0) {
+      return projects.map((project, index) => {
+        return <li>{project.name}</li>;
+      });
+    }
+  };
 
-        </section>
-    );
+  return (
+    <section className="projects">
+      <h1 className="projects__title">project menu</h1>
+      <div className="projects__detail"></div>
+      <ul className="projects__listNames">
+        {showProjectsName(projectObject)}
+      </ul>
+    </section>
+  );
 };
 
 export default Projects;
