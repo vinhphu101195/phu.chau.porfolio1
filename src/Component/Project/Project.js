38a0a237691fab2735c1b1270efc35a2ec012ac4
Phu Chau <vinhphu101195@gmail.com>
2019-10-30 15:05:02 +0200

    build the change of photo

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -1,15 +1,19 @@
-import React, { useContext, useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Project.css";
 import { ProjectContext } from "../../contexts/ProjectContexts";
 
 const Projects = () => {
   const { projectObject } = useContext(ProjectContext);
   const [popup, setPopup] = useState(projectObject[0]);
-  const [photo,setPhoto] = useState(popup.photo[0])
+  const [photoIndex, setPhotoIndex] = useState(0);
 
   const showSkills = skills => {
-    return skills.map((skill,index) => {
-      return <div key={index} className="project__details__skill">{skill}</div>;
+    return skills.map((skill, index) => {
+      return (
+        <div key={index} className="project__details__skill">
+          {skill}
+        </div>
+      );
     });
   };
 
@@ -21,7 +25,7 @@ const Projects = () => {
             key={index}
             onClick={() => {
               setPopup(project);
-              setPhoto(project.photo[0])
+              setPhotoIndex(0);
             }}
             className="projects__list__click"
           >
@@ -32,6 +36,17 @@ const Projects = () => {
     }
   };
 
+  function onclickRight() {
+    if (photoIndex < popup.photo.length - 1) {
+      setPhotoIndex(photoIndex + 1);
+    }
+  }
+  function onclickLeft() {
+    if (photoIndex > 0) {
+      setPhotoIndex(photoIndex - 1);
+    }
+  }
+
   console.log(popup);
 
   return (
@@ -41,13 +56,19 @@ const Projects = () => {
       </h1>
       <div className="projects__detail">
         <div className="projects__detail__photo ">
-          <i className="fas fa-chevron-left projects__icons"></i>
+          <i
+            className="fas fa-chevron-left projects__icons"
+            onClick={() => onclickLeft()}
+          ></i>
           <img
-            src={photo}
+            src={popup.photo[photoIndex]}
             className="projects__detail__img"
             alt="example of project"
           />
-          <i className="fas fa-chevron-right projects__icons"></i>
+          <i
+            className="fas fa-chevron-right projects__icons"
+            onClick={() => onclickRight()}
+          ></i>
         </div>
         <div className="projects__details__links">
           {popup.sourcecode ? (
@@ -57,7 +78,7 @@ const Projects = () => {
               rel="noopener noreferrer "
               className="popup__link-source__detail"
             >
-              <span>Source Code</span>|
+              <span>Source Code </span>|
             </a>
           ) : (
             ""
